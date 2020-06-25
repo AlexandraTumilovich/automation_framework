4b916f8b926d2c85ee5e990b703613b1ccb3cc8c
Alexandra Tumilovich <darien24688@gmail.com>
2020-06-25 21:03:47 +0300

    first

--- src/test/java/project/by/stormnet/functional/tests/LamodaSearchTest.java
+++ src/test/java/project/by/stormnet/functional/tests/LamodaSearchTest.java
@@ -1,6 +1,7 @@
 package project.by.stormnet.functional.tests;
 
 import org.testng.Assert;
+import org.testng.SkipException;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 import project.by.stormnet.functional.entities.helpers.HomeHelper;
@@ -13,7 +14,21 @@ public class LamodaSearchTest {
     public void performSearch(){
         int countPerPage = homeHelper.search(searchKey).getSearchResultsCountPerPage();
         Assert.assertTrue(countPerPage > 0, "No results were found");
-        //Assert.assertFalse(countPerPage > 0, "No results were found");
+    }
+
+    @Test
+    public void testPassed(){
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void testSkipped(){
+        throw new SkipException("This test is skipped");
+    }
+
+    @Test
+    public void testFailed(){
+        Assert.assertTrue(false);
     }
 
     @AfterClass
